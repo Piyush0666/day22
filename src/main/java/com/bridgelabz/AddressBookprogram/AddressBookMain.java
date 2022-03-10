c1c801e71874c4002708ec5c781e972abb5da659
your_github_username <your_github_email>
2022-03-10 17:17:35 +0530

    welcome message

---
+++ src/main/java/com/bridgelabz/AddressBookprogram/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.AddressBookprogram;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO ADDRESS BOOK PROGRAM");
+    }
+}
---
+++ src/test/java/com/bridgelabz/AddressBookprogram/AddressBookMainTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.AddressBookprogram;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookMainTest
+{
+        @Test
+        public void shouldAnswerWithTrue()
+        {
+            assertTrue( true );
+        }
+
+}
